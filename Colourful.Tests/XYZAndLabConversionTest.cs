22a3d50a87482602cec28dc56cbf4b6a80cb62e7
Tomáš Pažourek <tom.pazourek@gmail.com>
2013-08-31 12:16:25 +0200

    Ammend last commit.

---
+++ Colourful.Tests/XYZAndLabConversionTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colourful.Colors;
+using NUnit.Framework;
+
+namespace Colourful.Tests
+{
+    /// <summary>
+    /// Tests XYZ-LAB conversions
+    /// </summary>
+    /// <remarks>
+    /// Test data generated using:
+    /// http://www.brucelindbloom.com/index.html?ColorCalculator.html
+    /// </remarks>
+    [TestFixture]
+    public class XYZAndLabConversionTest
+    {
+        private static readonly IComparer<double> DoubleComparerLabPrecision = new DoubleRoundingComparer(4);
+        private static readonly IComparer<double> DoubleComparerXYZPrecision = new DoubleRoundingComparer(6);
+
+        /// <summary>
+        /// Tests conversion from XYZ (D65) to L*a*b*
+        /// </summary>
+        [Test]
+        [TestCase(0.95047, 1, 1.08883, 100, 0, 0)]
+        [TestCase(0, 0, 0, 0, 0, 0)]
+        [TestCase(0.95047, 0, 0, 0, 431.0345, 0)]
+        [TestCase(0, 1, 0, 100, -431.0345, 172.4138)]
+        [TestCase(0, 0, 1.08883, 0, 0, -172.4138)]
+        [TestCase(0.216938, 0.150041, 0.048850, 45.6398, 39.8753, 35.2091)]
+        public void Convert_XYZ_to_LAB(double x, double y, double z, double l, double a, double b)
+        {
+            // arrange
+            var input = new XYZColor(x, y, z, Illuminants.D65);
+
+            // act
+            LabColor output = input.ToLab();
+
+            // assert
+            Assert.That(output.L, Is.EqualTo(l).Using(DoubleComparerLabPrecision));
+            Assert.That(output.a, Is.EqualTo(a).Using(DoubleComparerLabPrecision));
+            Assert.That(output.b, Is.EqualTo(b).Using(DoubleComparerLabPrecision));
+        }
+
+        /// <summary>
+        /// Tests conversion from L*a*b* to XYZ (D65)
+        /// </summary>
+        [Test]
+        [TestCase(100, 0, 0, 0.95047, 1, 1.08883)]
+        [TestCase(0, 0, 0, 0, 0, 0)]
+        [TestCase(0, 431.0345, 0, 0.95047, 0, 0)]
+        [TestCase(100, -431.0345, 172.4138, 0, 1, 0)]
+        [TestCase(0, 0, -172.4138, 0, 0, 1.08883)]
+        [TestCase(45.6398, 39.8753, 35.2091, 0.216938, 0.150041, 0.048850)]
+        [TestCase(77.1234, -40.1235, 78.1120, 0.358530, 0.517372, 0.076273)]
+        [TestCase(10, -400, 20, 0, 0.011260, 0)]
+        public void Convert_LAB_to_XYZ(double l, double a, double b, double x, double y, double z)
+        {
+            // arrange
+            var input = new LabColor(l, a, b);
+
+            // act
+            XYZColor output = input.ToXYZ(Illuminants.D65);
+
+            // assert
+            Assert.That(output.X, Is.EqualTo(x).Using(DoubleComparerXYZPrecision));
+            Assert.That(output.Y, Is.EqualTo(y).Using(DoubleComparerXYZPrecision));
+            Assert.That(output.Z, Is.EqualTo(z).Using(DoubleComparerXYZPrecision));
+        }
+    }
+}
---
+++ Colourful/Conversion/XYZAndLabConverter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colourful.Colors;
+
+namespace Colourful.Conversion
+{
+    /// <summary>
+    /// Converts from XYZ to L*a*b* and backwards
+    /// </summary>
+    public class XYZAndLabConverter : IColorConverter<XYZColor, LabColor>, IColorConverter<LabColor, XYZColor>
+    {
+        private const double Epsilon = 216d / 24389d;
+        private const double Kappa = 24389d / 27d;
+
+        #region XYZ to Lab
+
+        public LabColor Convert(XYZColor input)
+        {
+            // Conversion algorithm described here: http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_Lab.html
+            double Xr = input.ReferenceWhite.X, Yr = input.ReferenceWhite.Y, Zr = input.ReferenceWhite.Z;
+
+            double xr = input.X / Xr, yr = input.Y / Yr, zr = input.Z / Zr;
+
+            double fx = f(xr);
+            double fy = f(yr);
+            double fz = f(zr);
+
+            double L = 116 * fy - 16;
+            double a = 500 * (fx - fy);
+            double b = 200 * (fy - fz);
+
+            var output = new LabColor(L, a, b);
+            return output;
+        }
+
+        private double f(double cr)
+        {
+            double fc = cr > Epsilon ? Math.Pow(cr, 1 / 3d) : (Kappa * cr + 16) / 116d;
+            return fc;
+        }
+
+        #endregion
+
+        #region Lab to XYZ
+
+        /// <summary>
+        /// Target reference white is <see cref="XYZColor.DefaultReferenceWhite"/>
+        /// <seealso cref="Convert(LabColor, XYZColorBase)"/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public XYZColor Convert(LabColor input)
+        {
+            var result = Convert(input, XYZColor.DefaultReferenceWhite);
+            return result;
+        }
+
+        public XYZColor Convert(LabColor input, XYZColorBase referenceWhite)
+        {
+            // Conversion algorithm described here: http://www.brucelindbloom.com/index.html?Eqn_Lab_to_XYZ.html
+            double L = input.L, a = input.a, b = input.b;
+            double fy = (L + 16) / 116d;
+            double fx = a / 500d + fy;
+            double fz = fy - b / 200d;
+
+            double fx3 = Math.Pow(fx, 3);
+            double fz3 = Math.Pow(fz, 3);
+
+            double xr = fx3 > Epsilon ? fx3 : (116 * fx - 16) / Kappa;
+            double yr = L > Kappa * Epsilon ? Math.Pow((L + 16) / 116d, 3) : L / Kappa;
+            double zr = fz3 > Epsilon ? fz3 : (116 * fz - 16) / Kappa;
+
+            double Xr = referenceWhite.X, Yr = referenceWhite.Y, Zr = referenceWhite.Z;
+
+            // avoids XYZ coordinates out of XYZ ref. white range
+            xr = xr.CropRange(0, 1);
+            yr = yr.CropRange(0, 1);
+            zr = zr.CropRange(0, 1);
+
+            double X = xr * Xr;
+            double Y = yr * Yr;
+            double Z = zr * Zr;
+
+            var result = new XYZColor(X, Y, Z, referenceWhite);
+            return result;
+        }
+
+        #endregion
+    }
+}
